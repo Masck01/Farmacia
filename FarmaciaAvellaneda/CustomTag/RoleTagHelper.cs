9d77ee7683e9d4f42620af76a15f8cac12378251
Masck01 <lobojujenho@gmail.com>
2020-11-15 20:34:22 -0300

    -- Added Rols to Users -- Added CustomTag -- Solved Imports _ViewImports

---
+++ FarmaciaAvellaneda/CustomTag/RoleTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmaciaAvellaneda.CustomTag
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("td", Attributes = "i-role")]
+    public class RoleTagHelper : TagHelper
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RoleTagHelper(UserManager<IdentityUser> manager, RoleManager<IdentityRole> role)
+        {
+            _userManager = manager;
+            _roleManager = role;
+        }
+
+        [HtmlAttributeName("i-role")]
+        public string Role { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> names = new List<string>();
+            IdentityRole role = await _roleManager.FindByIdAsync(Role);
+            if (role != null)
+            {
+                foreach (var user in _userManager.Users)
+                {
+                    if (user != null && await _userManager.IsInRoleAsync(user, role.Name))
+                    {
+                        names.Add(user.UserName);
+                    }
+                }
+            }
+            output.Content.SetContent(names.Count == 0 ? "Sin usuarios" : string.Join(", ", names));
+        }
+    }
+}
